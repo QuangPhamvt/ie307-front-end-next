2e4d177bd2e9235c2dc9b765b5353aede762b134
QuangPhamvt <quangpham220503vt@gmail.com>
2023-12-12 09:52:42 +0700

    feat: #30  upload view

--- src/view/screens/Main/components/UploadTab/index.tsx
+++ src/view/screens/Main/components/UploadTab/index.tsx
@@ -1,26 +1,11 @@
+import { AntDesign } from "@expo/vector-icons"
 import React from "react"
-import { PermissionsAndroid, Text, View } from "react-native"
-import CameraRoll from "@react-native-camera-roll/camera-roll"
-
+import { Text, View } from "react-native"
+import HeaderComponent from "./components/HeaderComponent"
 const UploadTab: React.FC = () => {
-  const checkPermission = async () => {
-    const hasPermission = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE)
-    if (hasPermission) {
-      return true
-    }
-    const status = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE, {
-      title: "Image gallery app",
-      message: "Image gallery needs your permission to access your photos",
-      buttonPositive: "Ok",
-    })
-    return status === "granted"
-  }
-  const getPhots = async () => {
-    const photos = await CameraRoll.getPhotos({})
-  }
   return (
-    <View>
-      <Text>Upload tab</Text>
+    <View className="flex h-screen w-full bg-black">
+      <HeaderComponent />
     </View>
   )
 }
