a1eb549ac3ba79a6d130e24b6ce2f46e5b6405d2
Tom <120240787+QuangPhamvt@users.noreply.github.com>
2023-12-14 00:04:28 +0700

    feat: #43  add upload posts and search post list

--- src/view/screens/Main/components/SearchTab/components/SearchStack/components/ListPostComponent.tsx
+++ src/view/screens/Main/components/SearchTab/components/SearchStack/components/ListPostComponent.tsx
@@ -1,12 +1,15 @@
 import React from "react"
-import { View, ScrollView, TouchableOpacity } from "react-native"
+import { View, ScrollView, TouchableOpacity, NativeScrollEvent, ActivityIndicator } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
 import ImageHoc from "~/src/view/components/ImageHOC"
 import { SearchViewStackParamList } from "~/src/view/type"
+import SearchStackAction from "../store/hook"
+import { useRecoilValue } from "recoil"
+import { listPostSearchState } from "../store/atom"
 
 type TInstaGrid = {
-  data: { amountPosts: number; data: any[]; direction: string }[]
+  data: { amountPosts: number; data: { id: string; images: Array<string> }[]; direction: string }[]
   columns: number
   loading: boolean
   onEndReachedThreshold: number
@@ -24,11 +27,12 @@ type TGroup3nthPost = {
 const Group5nthPost: React.FC<TGroup5nthPost> = (props) => {
   const { data, direction } = props
   const navigation = useNavigation<StackNavigationProp<SearchViewStackParamList, "SearchStack">>()
+  data.map((item) => console.log(item))
   return (
-    <View className="flex h-auto w-screen flex-row flex-wrap">
+    <View className="flex flex-row flex-wrap w-screen h-auto">
       {direction === "right" && (
         <TouchableOpacity className="aspect-square w-1/3 border-[1px] border-black bg-slate-400">
-          <ImageHoc uri={data[3].uri} />
+          <ImageHoc uri={data[3] && data[3].images[0]} />
         </TouchableOpacity>
       )}
       <View className="flex aspect-square h-2/3 w-2/3 flex-row flex-wrap border-[1px] border-black">
@@ -36,25 +40,25 @@ const Group5nthPost: React.FC<TGroup5nthPost> = (props) => {
           onPress={() => navigation.navigate("PostDetailStack")}
           className="flex aspect-square h-1/2 w-1/2 items-center justify-center border-[1px] border-black"
         >
-          <ImageHoc uri={data[0].uri} />
+          <ImageHoc uri={data[0] && data[0].images[0]} />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square h-1/2 w-1/2 border-[1px] border-black"
         >
-          <ImageHoc uri={data[1].uri} />
+          <ImageHoc uri={data[1] && data[1].images[0]} />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square h-1/2 w-1/2 border-[1px] border-black"
         >
-          <ImageHoc uri={data[2].uri} />
+          <ImageHoc uri={data[2] && data[2].images[0]} />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square h-1/2 w-1/2 border-[1px] border-black"
         >
-          <ImageHoc uri={data[3].uri} />
+          <ImageHoc uri={data[3] && data[3].images[0]} />
         </TouchableOpacity>
       </View>
       {direction === "left" && (
@@ -62,7 +66,7 @@ const Group5nthPost: React.FC<TGroup5nthPost> = (props) => {
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square w-1/3 border-[1px] border-black bg-slate-400"
         >
-          <ImageHoc uri={data[3].uri} />
+          <ImageHoc uri={data[3] && data[3].images[0]} />
         </TouchableOpacity>
       )}
     </View>
@@ -71,14 +75,15 @@ const Group5nthPost: React.FC<TGroup5nthPost> = (props) => {
 const Group3nthPost: React.FC<TGroup3nthPost> = (props) => {
   const { data, direction } = props
   const navigation = useNavigation<StackNavigationProp<SearchViewStackParamList, "SearchStack">>()
+  console.log(data)
   return (
-    <View className="flex w-full flex-row flex-wrap ">
+    <View className="flex flex-row flex-wrap w-full ">
       {direction === "right" && (
         <TouchableOpacity
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square h-2/3 w-2/3 border-[1px] border-black bg-gray-500"
         >
-          <ImageHoc uri={data[2].uri} />
+          <ImageHoc uri={data[2] && data[2].images[0]} />
         </TouchableOpacity>
       )}
       <View className="flex w-1/3">
@@ -86,13 +91,13 @@ const Group3nthPost: React.FC<TGroup3nthPost> = (props) => {
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square w-full  border-[1px] border-black bg-gray-400"
         >
-          <ImageHoc uri={data[0].uri} />
+          <ImageHoc uri={data[0] && data[0].images[0]} />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square w-full  border-[1px] border-black bg-gray-500"
         >
-          <ImageHoc uri={data[1].uri} />
+          <ImageHoc uri={data[1] && data[1].images[0]} />
         </TouchableOpacity>
       </View>
       {direction === "left" && (
@@ -100,15 +105,29 @@ const Group3nthPost: React.FC<TGroup3nthPost> = (props) => {
           onPress={() => navigation.navigate("PostDetailStack")}
           className="aspect-square h-2/3 w-2/3 border-[1px] border-black bg-gray-500"
         >
-          <ImageHoc uri={data[2].uri} />
+          <ImageHoc uri={data[2] && data[2].images[0]} />
         </TouchableOpacity>
       )}
     </View>
   )
 }
 const InstaGrid: React.FC<TInstaGrid> = ({ data, onEndReached, loading = false, onItemClick }) => {
+  const { state } = useRecoilValue(listPostSearchState)
+  const { onGetListPost } = SearchStackAction.useGetListPost()
+  const isCloseToBottom = (nativeEvent: NativeScrollEvent) => {
+    const { layoutMeasurement, contentOffset, contentSize } = nativeEvent
+    return layoutMeasurement.height + contentOffset.y >= contentSize.height + 20
+  }
   return (
-    <ScrollView className="flex w-full">
+    <ScrollView
+      className="flex w-full"
+      scrollEventThrottle={400}
+      onScroll={({ nativeEvent }) => {
+        if (isCloseToBottom(nativeEvent)) {
+          onGetListPost()
+        }
+      }}
+    >
       {data.map((item, index) => {
         return (
           <View key={index} className="w-full">
@@ -117,6 +136,11 @@ const InstaGrid: React.FC<TInstaGrid> = ({ data, onEndReached, loading = false, 
           </View>
         )
       })}
+      {state === "loading" && (
+        <View className="mt-4">
+          <ActivityIndicator animating size={"large"} color={"black"} />
+        </View>
+      )}
     </ScrollView>
   )
 }
